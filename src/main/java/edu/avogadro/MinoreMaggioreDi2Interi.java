b81a9fcfeb2996fe94306fbf158acde2e3996bcf
Khali <vetralla.alberto@hotmail.it>
2023-09-30 14:30:29 +0200

    Initial commit

---
+++ src/main/java/edu/avogadro/MinoreMaggioreDi2Interi.java
@@ -0,0 +1,26 @@
+package edu.avogadro;
+
+import java.util.Scanner;
+
+public class MinoreMaggioreDi2Interi {
+    public static void main(String[] args) { // equivale a start
+        System.out.print(
+                  "**************\n"
+                + "Questo programma ti chiederà l'inserimento di 2 interi e visualizzerà qual'è il minore e quale il maggiore *\n"
+                + "e visualizzerà qual'è il minore e quale il maggior\n                                                       *"
+                + "************************************************************************************************************\n\n");
+        System.out.print("Inserisci il primo intero: ");
+
+        Scanner lettore = new Scanner(System.in);
+        int primoIntero = lettore.nextInt();
+
+        System.out.print("Inserisci il secondo intero: ");
+
+        int secondoIntero = lettore.nextInt();
+
+        System.out.println("Ho letto il primo numero intero = "+ primoIntero);
+        System.out.println("Ho letto il secondo intero = "+ secondoIntero);
+
+
+    }
+}
